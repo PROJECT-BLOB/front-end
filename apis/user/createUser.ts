538cbdb431f4f9f0c8cb66ab559921eed6f6f115
Duke <127701344+Dev-Duke-Seo@users.noreply.github.com>
2024-04-23 00:25:09 +0900

    fix 주석 수정

--- apis/user/createUser.ts
+++ apis/user/createUser.ts
@@ -1,6 +1,6 @@
 import instance from '@apis/axios';
 
-// 회원 탈퇴
+// 유저 아이디,닉네임 생성
 export default async function createUser(nickname: string, id: string) {
   const result = await instance.post('/user', {
     nickname,
