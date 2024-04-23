4fee43da9a756db6c1d045b8683d10325b45ddc8
yejiniee <02doyoung01@naver.com>
2024-04-23 15:21:01 +0900

    refactor: 코드리뷰 반영

--- apis/user/getUserPostList.ts
+++ apis/user/getUserPostList.ts
@@ -1,15 +1,22 @@
 import instance from '@apis/axios';
 
-interface Post {
+export interface Post {
   postId: number;
-  author: number;
+  authorId: number;
+  authorNickname: string;
   title: string;
   content: string;
   createdAt: string;
+  updatedAt: string;
+  likesCount: number;
+  commentsCount: number;
 }
 
-export default async function getUserPostList(userId: number): Promise<Post[]> {
-  const { data } = await instance.get<Post[]>(`/users/${userId}/posts`);
+export type PostSummary = Pick<Post, 'postId' | 'authorId' | 'authorNickname' | 'title' | 'content' | 'createdAt'>;
+
+// getUserPostList : 유저가 작성한 글 모음
+export default async function getUserPostList(userId: number): Promise<PostSummary[]> {
+  const { data } = await instance.get<PostSummary[]>(`/users/${userId}/posts`);
 
   return data;
 }
