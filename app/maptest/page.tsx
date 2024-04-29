f50de34aee69c9d4350a3158c9846ad5ec9d2248
INKmin9 <mkkim9901@naver.com>
2024-04-29 13:51:12 +0900

    feat: autocomplete 작성

--- app/maptest/page.tsx
+++ app/maptest/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import BasicMapAlpha from '@/app/maptest/BasicMapAlpha';
 
+import AutoComplete from './AutoComplete';
 import styles from './MapTest.module.scss';
 
 export default function MapTest() {
+  const [place, setPlace] = useState('');
+
   return (
     <div className={styles.body}>
       <h2>Map 페이지</h2>
+      <AutoComplete place={place} setPlace={setPlace} />
       <BasicMapAlpha />
 
       <div className={styles['button-container']}>
