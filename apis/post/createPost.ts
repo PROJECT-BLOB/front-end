42bbc3a3dce4b88905e25c7d1c80a29108fe0bf3
chlangus <angusangus11@gmail.com>
2024-04-29 16:49:44 +0900

    feat: post관련 api 구성

---
+++ apis/post/createPost.ts
@@ -0,0 +1,24 @@
+import { Post } from '@/types/Post';
+import instance from '@apis/axios';
+
+// interface CreatePost {
+//   data: {
+//     title: string;
+//     content: string;
+//     category: string;
+//     subcategory: string;
+//     country: string;
+//     city: string;
+//     lat: number;
+//     lng: number;
+//     actualLat: number;
+//     actualLng: number;
+//   };
+//   file: File;
+// }
+
+export default async function getPost(formData: FormData): Promise<{ data: Post; status: number }> {
+  const { data, status } = await instance.post(`/post`, formData);
+
+  return { data, status };
+}
