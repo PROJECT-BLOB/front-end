5bc042309b86bfb9ab9ff3bd0b590ba596162e71
chlangus <angusangus11@gmail.com>
2024-04-30 01:43:59 +0900

    fix: 린트 에러 제거

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -4,7 +4,6 @@ import useModalStore, { ModalName } from '@stores/useModalStore';
 
 import ReadPost from '@components/Modal/ReadPost/ReadPost';
 import WritePost from '@components/Modal/WritePost/WritePost';
-import Tab from '@components/Tab';
 
 export default function Map() {
   const { toggleModal, name, setCurrentName } = useModalStore();
@@ -17,8 +16,6 @@ export default function Map() {
   return (
     <>
       <h2>Map 페이지</h2>
-      <Tab isClicked={false}>탭 요</Tab>
-      <Tab isClicked>선택 탭 요</Tab>
       <button type='button' onClick={() => handleClickModal('write')}>
         작성하기
       </button>
