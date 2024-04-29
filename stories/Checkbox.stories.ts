f3691d64dda4b65f809cc74fcab5990ef9533a61
yejiniee <02doyoung01@naver.com>
2024-04-29 22:34:41 +0900

    feat: 라디오버튼 스토리북 생성

--- stories/Checkbox.stories.ts
+++ stories/Checkbox.stories.ts
@@ -4,43 +4,18 @@ export default {
   title: 'Design System/Checkbox',
   component: Checkbox,
   argTypes: {
-    checkedItemHandler: { action: 'checked' }, // 이벤트 핸들러 액션 추가
+    checkedItemHandler: { action: 'checkedItemHandler' },
+    disabled: { control: 'boolean', description: '체크박스를 클릭하지 못하도록 함' },
   },
 };
 
-// const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
-
-// export const Unselected = Template.bind({});
-export const Unselected = {
+export const Default = {
   args: {
-    value: 'Unselected',
-    children: '체크박스-unselected',
-    // checkedItemHandler: (value, isChecked) => console.log(`Checkbox value: ${value}, isChecked: ${isChecked}`),
+    value: 'default',
+    children: '체크박스-Default',
   },
 };
 
-// export const Disabled = Template.bind({});
 export const Disabled = {
-  args: { value: 'Disabled', children: '체크박스-Disabled' },
+  args: { value: 'disabled', children: '체크박스-Disabled', disabled: true },
 };
-
-// export const AlreadySelected = Template.bind({});
-// AlreadySelected.args = {
-//   value: 'unselected',
-//   children: 'Checkbox Label',
-//   checkedItemHandler: (value, isChecked) => console.log(`Checkbox value: ${value}, isChecked: ${isChecked}`),
-// };
-
-// export const Indeterminated = Template.bind({});
-// Indeterminated.args = {
-//   value: 'unselected',
-//   children: 'Checkbox Label',
-//   checkedItemHandler: (value, isChecked) => console.log(`Checkbox value: ${value}, isChecked: ${isChecked}`),
-// };
-
-// export const Selected = Template.bind({});
-// Selected.args = {
-//   value: 'unselected',
-//   children: 'Checkbox Label',
-//   checkedItemHandler: (value, isChecked) => console.log(`Checkbox value: ${value}, isChecked: ${isChecked}`),
-// };
