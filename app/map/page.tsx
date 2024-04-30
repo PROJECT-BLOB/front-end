00ae48f832f4910457761cde6bb80641ea52f97a
INKmin9 <mkkim9901@naver.com>
2024-04-30 15:12:45 +0900

    feat: 버튼 컴포넌트 스타일링

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,6 +2,7 @@
 
 import useModalStore, { ModalName } from '@stores/useModalStore';
 
+import Button from '@components/Button/Button';
 import ReadPost from '@components/Modal/ReadPost/ReadPost';
 import WritePost from '@components/Modal/WritePost/WritePost';
 
@@ -23,6 +24,11 @@ export default function Map() {
       <button type='button' onClick={() => handleClickModal('read')}>
         읽기
       </button>
+      <Button text='BLOB' type='button' color='button-colord-contain' onClick={() => handleClickModal('write')} />
+      <Button text='BLOB' type='button' color='button-colord-outlined' onClick={() => handleClickModal('write')} />
+      <Button text='BLOB' type='button' color='button-gray-contain' onClick={() => handleClickModal('write')} />
+      <Button text='취소' type='button' color='button-gray-outlined' onClick={() => handleClickModal('write')} />
+
       {name === 'write' ? <WritePost /> : <ReadPost />}
     </>
   );
