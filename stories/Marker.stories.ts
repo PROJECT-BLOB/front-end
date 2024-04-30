6d8faa4001f25ff7242cd32a994b3a604c0c40ce
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2024-05-01 03:57:42 +0900

    feat: 공용컴포넌트(marker)부분 스토리북 작성

--- stories/Marker.stories.ts
+++ stories/Marker.stories.ts
@@ -1,62 +1,34 @@
-import { fn } from '@storybook/test';
-
-import { Button } from './Button';
+import Marker, { MarkerType } from '@/app/map/components/Marker';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Map/Marker',
+  component: Marker,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: [],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // opacity: { control: 'number', min: 25, max: 100 },
+    opacity: { control: 'select', options: [25, 50, 75, 100] },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+  args: {},
+} satisfies Meta<typeof Marker>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Marker>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Marker_Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
-
-export const Warning: Story = {
-  args: {
-    primary: true,
-    label: 'Delete now',
-    backgroundColor: 'red',
+    markerType: 'recommendation' as MarkerType,
+    opacity: 100,
   },
 };
