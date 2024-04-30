1f716400cb9cb8f17add0a7bf7dcb1d340150ef3
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2024-04-30 19:23:21 +0900

    fix: Marker 렌더링시 아이콘컴포넌트를 렌더링 하지 않도록 변경

--- app/map/components/Marker.tsx
+++ app/map/components/Marker.tsx
@@ -1,9 +1,10 @@
+import classNames from 'classnames/bind';
+
 import AtomIcon from '@icons/atom-01.svg?component';
 import MagicWandIcon from '@icons/magic-wand-02.svg?component';
 import SignalIcon from '@icons/signal-02.svg?component';
 import ThumbDownIcon from '@icons/thumbs-down.svg?component';
 import ThumbsUpIcon from '@icons/thumbs-up.svg?component';
-import classNames from 'classnames/bind';
 
 import styles from './Marker.module.scss';
 
@@ -17,24 +18,24 @@ interface MarkerProps {
   opacity?: 25 | 50 | 75 | 100;
 }
 
-export default function Marker({ markerType = 'recommendation', opacity = 100 }: MarkerProps) {
-  const SelectedIcon = (markerType: MarkerType) => {
-    switch (markerType) {
-      case 'recommendation':
-        return <ThumbsUpIcon className={cx('svg', 'thin')} />;
-      case 'blame':
-        return <ThumbDownIcon className={cx('svg', 'thin')} />;
-      case 'question':
-        return <AtomIcon className={cx('svg')} />;
-      case 'caution':
-        return <SignalIcon className={cx('svg')} />;
-      case 'help':
-        return <MagicWandIcon className={cx('svg')} />;
-      default:
-        return <SignalIcon className={cx('svg')} />;
-    }
-  };
+const SelectedIcon = (markerType: MarkerType) => {
+  switch (markerType) {
+    case 'recommendation':
+      return <ThumbsUpIcon className={cx('svg', 'thin')} />;
+    case 'blame':
+      return <ThumbDownIcon className={cx('svg', 'thin')} />;
+    case 'question':
+      return <AtomIcon className={cx('svg')} />;
+    case 'caution':
+      return <SignalIcon className={cx('svg')} />;
+    case 'help':
+      return <MagicWandIcon className={cx('svg')} />;
+    default:
+      return <SignalIcon className={cx('svg')} />;
+  }
+};
 
+export default function Marker({ markerType = 'recommendation', opacity = 100 }: MarkerProps) {
   return (
     <div className={cx('background', `color-${markerType}`, `opacity-${opacity}`)}>{SelectedIcon(markerType)}</div>
   );
