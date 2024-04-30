acabd4ae86212c9eb4ed86b9bb84101fa5178f5c
INKmin9 <mkkim9901@naver.com>
2024-04-30 15:23:27 +0900

    feat: 스토리북 작성중

---
+++ stories/BlobButton.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import Button, { ButtonProps } from '../components/Button/Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
+} as Meta;
+
+const Template: StoryFn<ButtonProps> = (args: ButtonProps) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'BLOB',
+  type: 'button',
+  color: 'button-colord-contain',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  text: '취소',
+  type: 'button',
+  color: 'button-gray-outlined',
+};
