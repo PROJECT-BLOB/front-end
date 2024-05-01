d45533404a10a7a1dee0f52ddafcbe5fe1f81fe9
INKmin9 <mkkim9901@naver.com>
2024-05-01 16:34:26 +0900

    feat: input 요소들 컴포넌트화

--- components/Modal/WritePost/WritePost.tsx
+++ components/Modal/WritePost/WritePost.tsx
@@ -2,39 +2,35 @@ import useModalStore from '@stores/useModalStore';
 
 import ImageUploader from '@components/ImageUploader';
 
+import CloseHeader from './CloseHeader';
+import ContentInput from './ContentInput';
+import TitleInput from './TitleInput';
 import styles from './WritePost.module.scss';
 import useCreateForm from '../../../app/map/_hooks/useCreateForm';
+import BlobButton from '../../Button/BlobButton';
 import Modal from '../Modal';
 
 export default function WritePost() {
   const { toggleModal } = useModalStore();
-  const { register, handleSubmit, onSubmit, cancelForm, setValue } = useCreateForm(toggleModal);
+  const { handleSubmit, onSubmit, cancelForm, setValue } = useCreateForm(toggleModal);
   // TODO: 카테고리는 구체화 될때까지 보류, 위치도 보류
 
   return (
     <Modal>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Modal.Header>
-          <div className={styles.postHeader}>
-            <span>글 작성</span>
-            <span>X</span>
-          </div>
+          <CloseHeader />
         </Modal.Header>
         <Modal.Body>
-          <label htmlFor='title'>title</label>
-          <input id='title' {...register('title')} type='text' placeholder='제목' className={styles.input} />
-          <label htmlFor='content'>content</label>
-          <input id='content' {...register('content')} type='text' placeholder='내용' className={styles.input} />
+          <div>카테고리</div>
+          <TitleInput />
+          <ContentInput />
           <ImageUploader setValue={setValue} />
         </Modal.Body>
         <Modal.Footer>
           <div className={styles.postFooter}>
-            <button type='button' onClick={cancelForm} className={styles.cancelButton}>
-              취소
-            </button>
-            <button type='submit' className={styles.submitButton}>
-              완료
-            </button>
+            <BlobButton text='취소' type='button' color='button-gray-outlined' onClick={cancelForm} />
+            <BlobButton text='BLOB' type='submit' color='button-colord-contain' />
           </div>
         </Modal.Footer>
       </form>
