1d0744d3ea4b8ee6e1187e4145b9118386269655
yejiniee <02doyoung01@naver.com>
2024-05-04 19:35:26 +0900

    remove: 필요없는 주석 제거

--- app/mypage/_components/Post/PostList.tsx
+++ app/mypage/_components/Post/PostList.tsx
@@ -1,11 +1,3 @@
-// import { Post } from '@/types/Post';
-
-// import PostItem from './PostItem';
-
-// export default function PostList({ postList }: { postList: Post[] }) {
-//   return <>{postList && postList.map((post: Post) => <PostItem key={post.postId} post={post} />)}</>;
-// }
-// PostList.tsx
 import classNames from 'classnames/bind';
 
 import { Post } from '@/types/Post';
--- types/User.ts
+++ types/User.ts
@@ -3,16 +3,16 @@ export interface UserDetail {
   email: string;
   blobId: string;
   nickName: string;
-  bio: 'string';
+  bio: string;
   profileUrl: string;
   state: 'COMPLETE' | 'INCOMPLETE';
   postCount: 0;
   likedCount: 0;
-  isPrivate: true;
+  isPrivate: boolean;
   coordinate: {
     lat: 0;
     lng: 0;
   };
-  oauthType: 'GOOGLE';
-  role: 'ROLE_USER';
+  oauthType: 'GOOGLE' | 'KAKAO' | 'NAVER';
+  role: 'ROLE_USER' | 'ROLE_ADMIN';
 }
