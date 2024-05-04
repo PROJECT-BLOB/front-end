c03b1483152207756993b29c9521c24f82b95951
yejiniee <02doyoung01@naver.com>
2024-05-05 01:10:34 +0900

    style: 사이즈 조정

--- components/Modal/CreateUser/CreateUser.tsx
+++ components/Modal/CreateUser/CreateUser.tsx
@@ -11,7 +11,6 @@ import useModalStore from '@stores/useModalStore';
 import BlobButton from '@components/Button/BlobButton';
 
 import styles from './CreateUser.module.scss';
-import Modal from '../Modal';
 
 const cx = classNames.bind(styles);
 
@@ -39,36 +38,34 @@ export default function CreateUser() {
 
   // TODO: id, nickname 유효성 검사 추가
   return (
-    <Modal>
-      <form className={cx('form')} onSubmit={handleSubmit(() => onSubmit(userFormData))}>
-        <header className={cx('header')}>
-          <span>회원가입</span>
-          <span className={cx('close')}>
-            <Image src={CloseButton} fill alt='닫기' onClick={cancelForm} />
-          </span>
-        </header>
+    <form className={cx('form')} onSubmit={handleSubmit(() => onSubmit(userFormData))}>
+      <header className={cx('header')}>
+        <span>회원가입</span>
+        <span className={cx('close')}>
+          <Image src={CloseButton} fill alt='닫기' onClick={cancelForm} />
+        </span>
+      </header>
 
-        <main className={cx('input')}>
-          <Input labelName='아이디' id='id' name='id' value={userFormData.id} onChange={handleChangeInput} />
-          <Input
-            labelName='닉네임'
-            id='nickname'
-            name='nickname'
-            value={userFormData.nickname}
-            onChange={handleChangeInput}
-          />
-        </main>
+      <main className={cx('input')}>
+        <Input labelName='아이디' id='id' name='id' value={userFormData.id} onChange={handleChangeInput} />
+        <Input
+          labelName='닉네임'
+          id='nickname'
+          name='nickname'
+          value={userFormData.nickname}
+          onChange={handleChangeInput}
+        />
+      </main>
 
-        <footer className={cx('buttons')}>
-          {/* <button type='button' onClick={cancelForm}>
+      <footer className={cx('buttons')}>
+        {/* <button type='button' onClick={cancelForm}>
             취소
           </button> */}
-          {/* <button type='submit'>회원가입</button> */}
-          <BlobButton text='취소' type='button' color='button-gray-outlined' onClick={cancelForm} />
-          {/* <BlobButton text='회원가입' type='submit' color='button-colord-contain' /> */}
-          <button type='submit'>회원가입</button>
-        </footer>
-      </form>
-    </Modal>
+        {/* <button type='submit'>회원가입</button> */}
+        <BlobButton text='취소' type='button' color='button-gray-outlined' onClick={cancelForm} />
+        {/* <BlobButton text='회원가입' type='submit' color='button-colord-contain' /> */}
+        <button type='submit'>회원가입</button>
+      </footer>
+    </form>
   );
 }
