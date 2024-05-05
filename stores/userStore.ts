d7565387c5a9691f501e23742609f25299523581
yejiniee <02doyoung01@naver.com>
2024-05-05 17:20:30 +0900

    fix: 마이페이지 초기 로딩 때 필요한 값이 안 들어오는 문제 해결

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -22,14 +22,19 @@ interface Location {
 export const useUserStore = create(
   persist<UserStore>(
     (set) => ({
-      userId: 0,
+      userId: typeof window !== 'undefined' ? Number(localStorage.getItem('userId')) : 0, // 초기 로딩 때 로컬스토리지에서 값을 읽어오도록 함
       isSignin: typeof window !== 'undefined' ? localStorage.getItem('isSignin') === 'true' : false, // 초기 로딩 때 로컬스토리지에서 값을 읽어오도록 함
       lastLocation: { lat: 0, lng: 0 },
-      setUserId: (newUserId: number) => set({ userId: newUserId }),
+      setUserId: (newUserId) => {
+        localStorage.setItem('userId', String(newUserId)); // 초기 로딩때 가져와야 하기 때문에 로컬스토리지에 값 저장
+        set({ userId: newUserId });
+      },
       signin: () => {
+        localStorage.setItem('isSignin', 'true');
         set(() => ({ isSignin: true }));
       },
       signout: () => {
+        localStorage.setItem('isSignin', 'false');
         set(() => ({ isSignin: false }));
       },
     }),
