99e60c5910774c276de5ee7310067a5073f2810d
yejiniee <02doyoung01@naver.com>
2024-05-08 14:36:20 +0900

    refactor: postlist 컴포넌트 리팩토링

--- app/mypage/_components/Post/PostList.tsx
+++ app/mypage/_components/Post/PostList.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-
+// postlist 안에서 데이터 불러오는 버전
 import classNames from 'classnames/bind';
 
 import { Comment, Post } from '@/types/Post';
+import { useFetchBookmarkList, useFetchCommentList, useFetchPostList } from '@queries/usePostQueries';
 
 import PostItem from './PostItem';
 import styles from './PostList.module.scss';
@@ -10,93 +10,64 @@ import CommentItem from '../Comment/CommentItem';
 
 const cx = classNames.bind(styles);
 
-interface PostListProps {
-  postsData: any;
-  isCommentList?: boolean;
-  isFetchingNextPage: boolean;
+interface GetPostListProps {
+  userId?: number;
+  selectedTab?: string;
+  // 타입 수정해주십셔...
+  filteredData?: any;
 }
 
-const ForwardedPostList = React.forwardRef<HTMLDivElement, PostListProps>(function PostListForwarded(
-  { postsData, isCommentList, isFetchingNextPage }: PostListProps,
-  ref,
-) {
-  const postsPages = postsData?.pages ?? [];
+export default function PostList({ userId, selectedTab, filteredData }: GetPostListProps) {
+  let fetchDataFunction;
+  switch (selectedTab) {
+    case 'MyPosts':
+      fetchDataFunction = useFetchPostList;
+      break;
+    case 'Bookmarks':
+      fetchDataFunction = useFetchBookmarkList;
+      break;
+    case 'MyComments':
+      fetchDataFunction = useFetchCommentList;
+      break;
+    // @queries/usePostQueries에 feedData 가져오는 쿼리 추가해서 아래 주석 제거하고 사용하시면 될 것 같습니다!!(이름은 임시로 지어둠)
+    // case 'Feed':
+    // fetchDataFunction=useFetchFeedList;
+    // break;
+    default:
+      fetchDataFunction = useFetchPostList; // 기본값으로 내가 쓴 글을 가져오도록 설정함
+      break;
+  }
+
+  const { data, isPending, isError, isFetchingNextPage, ref } = userId
+    ? fetchDataFunction(userId)
+    : fetchDataFunction(filteredData);
+
+  if (isPending) {
+    // TODO 스켈레톤 UI 추가
+    return <div>loading...</div>;
+  }
+
+  if (isError) {
+    return <div>데이터 불러오는 중, 에러 발생</div>;
+  }
+
+  const postsPages = data?.pages ?? [];
+  console.log('데이터', postsPages);
 
   return (
     <div className={cx('container')}>
-      {isCommentList
-        ? postsPages.map((postsPage: any) =>
+      {fetchDataFunction === useFetchCommentList
+        ? postsPages.map((postsPage) =>
             // TODO: post-title, category 정보도 같이 보내야 함
             postsPage.data.content.map((post: Comment) => <CommentItem key={post.commentId} post={post} />),
           )
-        : postsPages.map((postsPage: any) =>
+        : postsPages.map((postsPage) =>
             postsPage.data.content.map((post: Post) => <PostItem key={post.postId} post={post} />),
           )}
-      {/* //TODO: 무한스크롤이 또 안되는 현상 발생..ㅠㅠ 제 pc에서만 이상한거 같기도 하고 모르겠네요ㅠ */}
-      {isFetchingNextPage ? <div className={styles.loading}>로딩 중...</div> : <div ref={ref}>dd</div>}
+
+      {/*  TODO 로딩 인디케이터 추가 */}
+
+      {isFetchingNextPage ? <div>로딩 중...</div> : <div ref={ref} />}
     </div>
   );
-});
-
-export default ForwardedPostList;
-
-// postlist 안에서 데이터 불러오는 버전
-// import classNames from 'classnames/bind';
-
-// import { Comment, Post } from '@/types/Post';
-// import { useFetchBookmarkList, useFetchCommentList, useFetchPostList } from '@queries/usePostQueries';
-
-// import PostItem from './PostItem';
-// import styles from './PostList.module.scss';
-// import CommentItem from '../Comment/CommentItem';
-
-// const cx = classNames.bind(styles);
-
-// export default function PostList({ userId, selectedTab }: { userId: number; selectedTab: string }) {
-//   let fetchDataFunction;
-//   switch (selectedTab) {
-//     case 'MyPosts':
-//       fetchDataFunction = useFetchPostList;
-//       break;
-//     case 'Bookmarks':
-//       fetchDataFunction = useFetchBookmarkList;
-//       break;
-//     case 'MyComments':
-//       fetchDataFunction = useFetchCommentList;
-//       break;
-//     default:
-//       fetchDataFunction = useFetchPostList; // 기본값으로 내가 쓴 글을 가져오도록 설정
-//       break;
-//   }
-
-//   const { data, isPending, isError, isFetchingNextPage, ref } = fetchDataFunction(userId);
-
-//   if (isPending) {
-//     // TODO 스켈레톤 UI 추가
-//     return <div>loading...</div>;
-//   }
-
-//   if (isError) {
-//     return <div>데이터 불러오는 중, 에러 발생</div>;
-//   }
-
-//   const postsPages = data?.pages ?? [];
-//   console.log('데이터', postsPages);
-
-//   return (
-//     <div className={cx('container')}>
-//       {fetchDataFunction === useFetchCommentList
-//         ? postsPages.map((postsPage) =>
-//             // TODO: post-title, category 정보도 같이 보내야 함
-//             postsPage.data.content.map((post: Comment) => <CommentItem key={post.commentId} post={post} />),
-//           )
-//         : postsPages.map((postsPage) =>
-//             postsPage.data.content.map((post: Post) => <PostItem key={post.postId} post={post} />),
-//           )}
-
-//       {/*  TODO 로딩 인디케이터 추가 */}
-
-//       {isFetchingNextPage ? <div>로딩 중...</div> : <div ref={ref} />}
-//     </div>
-//   );
-// }
+}
--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -5,7 +5,6 @@ import { useEffect } from 'react';
 import classNames from 'classnames/bind';
 import { useRouter } from 'next/navigation';
 
-import { useFetchBookmarkList, useFetchCommentList, useFetchPostList } from '@queries/usePostQueries';
 import { useUserStore } from '@stores/userStore';
 import { useTabStore } from '@stores/useTabStore';
 
@@ -27,35 +26,6 @@ export default function myPage() {
     }
   }, [isSignin, router]);
 
-  // TODO: 리팩토링할지도..---------------
-  let fetchDataFunction;
-  switch (selectedTab) {
-    case 'MyPosts':
-      fetchDataFunction = useFetchPostList;
-      break;
-    case 'Bookmarks':
-      fetchDataFunction = useFetchBookmarkList;
-      break;
-    case 'MyComments':
-      fetchDataFunction = useFetchCommentList;
-      break;
-    default:
-      fetchDataFunction = useFetchPostList; // 기본값으로 내가 쓴 글을 가져오도록 설정
-      break;
-  }
-
-  const { data, isPending, isError, isFetchingNextPage, ref } = fetchDataFunction(userId);
-
-  if (isPending) {
-    // TODO 스켈레톤 UI 추가
-    return <div>loading...</div>;
-  }
-
-  if (isError) {
-    return <div>데이터 불러오는 중, 에러 발생</div>;
-  }
-  // TODO: 리팩토링할지도..--------------
-
   return (
     <div className={cx('wrappper')}>
       <section>
@@ -65,14 +35,7 @@ export default function myPage() {
         <TabList />
       </section>
       <section className={cx('post-list')}>
-        {/* postlist 안에서 데이터 불러오는 버전 */}
-        {/* <PostList userId={userId} selectedTab={selectedTab} /> */}
-        <PostList
-          isCommentList={selectedTab === 'MyComments'}
-          postsData={data}
-          isFetchingNextPage={isFetchingNextPage}
-          ref={ref}
-        />
+        <PostList userId={userId} selectedTab={selectedTab} />
       </section>
     </div>
   );
