bcd616de666d4fb79f52c9aa192ad4611d9ef128
INKmin9 <mkkim9901@naver.com>
2024-05-09 17:18:27 +0900

    feat: 자동완성에서 나오는 위경도 값 추출중..

--- app/map/_hooks/useCreateForm.ts
+++ app/map/_hooks/useCreateForm.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import createPost from '@apis/post/createPost';
@@ -5,11 +6,19 @@ import createPost from '@apis/post/createPost';
 export interface ContentField {
   title: string;
   content: string;
+  lat: number;
+  lng: number;
+  cityName: string;
   image: string[];
 }
 
+// 수정된 onSubmit 함수
+type LatLngLiteralOrNull = google.maps.LatLngLiteral | null;
+
 export default function useCreateForm(toggleModal: () => void) {
   const { register, handleSubmit, reset, setValue } = useForm<ContentField>();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [currentPosition, setCurrentPosition] = useState<LatLngLiteralOrNull>(null);
 
   function cancelForm() {
     reset();
@@ -18,14 +27,20 @@ export default function useCreateForm(toggleModal: () => void) {
 
   async function onSubmit(formData: ContentField) {
     try {
-      const formDataToSend = new FormData();
-      formDataToSend.append('title', formData.title);
-      formDataToSend.append('content', formData.content);
-      formData.image.forEach((image, index) => {
-        formDataToSend.append(`image${index}`, image);
-      });
-      await createPost(formDataToSend);
-      console.log('Post created successfully');
+      if (currentPosition) {
+        const formDataToSend = new FormData();
+        formDataToSend.append('title', formData.title);
+        formDataToSend.append('content', formData.content);
+        formDataToSend.append('lat', String(currentPosition.lat));
+        formDataToSend.append('lng', String(currentPosition.lng));
+        formData.image.forEach((image, index) => {
+          formDataToSend.append(`image${index}`, image);
+        });
+        await createPost(formDataToSend);
+        console.log('Post created successfully');
+      } else {
+        console.error('Current position is null');
+      }
     } catch (error) {
       console.error('Error creating post:', error);
     }
--- components/Modal/WritePost/WritePost.tsx
+++ components/Modal/WritePost/WritePost.tsx
@@ -1,37 +1,34 @@
-import { useState } from 'react';
+import React from 'react'; // 안하면 오류가 나더라구용..
 
 import classNames from 'classnames/bind';
 import Image from 'next/image';
 
-// import AutoCompleteCity from '@/app/maptest/_deprecated/autoCompleteCity';
-import useCreateForm from '@/app/map/_hooks/useCreateForm';
+import AutoCompleteCity from '@/app/maptest/_deprecated/AutoCompleteCity';
 import CloseButton from '@/public/icons/x-close.svg';
 import useModalStore from '@stores/useModalStore';
 
-// import CategoryFiltering from '@components/Category/CategoryFiltering';
+import CategoryFiltering, { Category } from '@components/Category/CategoryFiltering';
+import SubCategoryFiltering from '@components/Category/SubCategoryFiltering';
 import ImageUploader from '@components/ImageUploader';
 import Input from '@components/Input/Input';
 
 import PositionDetail from './PositionDetail';
 import styles from './WritePost.module.scss';
+import useCreateForm from '../../../app/map/_hooks/useCreateForm';
 import BlobButton from '../../Button/BlobButton';
 
 const cx = classNames.bind(styles);
 
+const categories: Category[] = ['추천', '비추천', '질문', '주의', '도움요청'];
+
+const subCategories = ['날씨', '음식점', '숙소', '병원', '화장실', '약국', '교통', '박물관', '관광지', 'ATM'];
+
 export default function WritePost() {
   const { toggleModal } = useModalStore();
-  const { handleSubmit, onSubmit, cancelForm, setValue } = useCreateForm(toggleModal);
-  const [titleInputValue, setTitleInputValue] = useState('');
+  const { register, handleSubmit, onSubmit, cancelForm, setValue } = useCreateForm(toggleModal);
 
-  // 제목이 없으면 blob를 눌러도 제출이 되지 않게 했습니다
-
-  const handleTitleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitleInputValue(event.target.value);
-  };
-
-  // const onSelectCity = (city: string) => {
-  //   console.log(`Selected city: ${city}`);
-  // };
+  // 선택된 도시의 정보를 담을 상태 변수
+  const [selectedCity, setSelectedCity] = React.useState<{ cityName: string; lat: number; lng: number } | null>(null);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={cx('form')}>
@@ -45,30 +42,42 @@ export default function WritePost() {
         <p className={cx('category-title')}>
           카테고리<span className={cx('force')}> * </span>
         </p>
+        <div className={cx('category-list')}>
+          {categories.map((category) => (
+            <CategoryFiltering key={category} category={category} filteringType='feed' />
+          ))}
+        </div>
 
-        {/* <CategoryFiltering categoryFilteringType='recommendation' title='추천' />
-        <CategoryFiltering categoryFilteringType='blame' title='비추천' />
-        <CategoryFiltering categoryFilteringType='question' title='질문' />
-        <CategoryFiltering categoryFilteringType='caution' title='주의' />
-        <CategoryFiltering categoryFilteringType='help' title='도움' /> */}
-        <Input
-          labelName='제목'
-          id='title'
-          name='title'
-          placeholder='제목은 필수입니다'
-          maxLength={20}
-          onChange={handleTitleInputChange}
-        />
+        {/* TODO 수정 필요 category별로 나눠야함  */}
+        <div className={cx('sub-category-list')}>
+          {subCategories.map((subcategory) => (
+            <SubCategoryFiltering key={subcategory} category='추천' filteringType='feed' title={subcategory} />
+          ))}
+        </div>
+        <Input labelName='제목' id='title' name='title' placeholder='제목은 필수입니다' maxLength={20} />
         <Input labelName='내용' id='content' name='content' placeholder='내용을 입력하세요' maxLength={20} />
         <p className={cx('city-title')}> 어디에 관한 글인가요? (도시까지)</p>
-        {/* <AutoCompleteCity onSelectCity={onSelectCity} /> */}
+        <AutoCompleteCity
+          onSelectCity={(cityName: string, lat: number, lng: number) => {
+            // 선택된 도시 정보를 상태 변수에 저장
+            setSelectedCity({ cityName, lat, lng });
+          }}
+        />
         <PositionDetail />
         <ImageUploader setValue={setValue} />
       </div>
-      <div className={styles.postFooter}>
+      <div className={cx('post-footer')}>
         <BlobButton text='취소' type='button' color='button-gray-outlined' onClick={cancelForm} />
-        <BlobButton text='BLOB' type='submit' color='button-colord-contain' disabled={!titleInputValue} />
+        <BlobButton text='BLOB' type='submit' color='button-colord-contain' />
       </div>
+      {/* 선택된 도시의 정보를 hidden input으로 추가하여 폼 데이터에 포함 */}
+      {selectedCity && (
+        <>
+          {/* <input type='hidden' {...register('cityName')} value={selectedCity.cityName} /> */}
+          <input type='hidden' {...register('lat')} value={selectedCity.lat} />
+          <input type='hidden' {...register('lng')} value={selectedCity.lng} />
+        </>
+      )}
     </form>
   );
 }
