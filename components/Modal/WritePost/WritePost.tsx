82f8e3886936b99df6469874843ab06a5d58e1f0
INKmin9 <mkkim9901@naver.com>
2024-05-10 17:21:14 +0900

    feat: form 데이터 보내기 중간커밋

--- components/Modal/WritePost/WritePost.tsx
+++ components/Modal/WritePost/WritePost.tsx
@@ -10,9 +10,9 @@ import useModalStore from '@stores/useModalStore';
 import CategoryFiltering, { Category } from '@components/Category/CategoryFiltering';
 import SubCategoryFiltering from '@components/Category/SubCategoryFiltering';
 import ImageUploader from '@components/ImageUploader';
-import Input from '@components/Input/Input';
 
 import PositionDetail from './PositionDetail';
+import PostModalInput from './PostModalInput';
 import styles from './WritePost.module.scss';
 import useCreateForm from '../../../app/map/_hooks/useCreateForm';
 import BlobButton from '../../Button/BlobButton';
@@ -25,7 +25,7 @@ const subCategories = ['날씨', '음식점', '숙소', '병원', '화장실', '
 
 export default function WritePost() {
   const { toggleModal } = useModalStore();
-  const { register, handleSubmit, onSubmit, cancelForm, setValue } = useCreateForm(toggleModal);
+  const { errors, register, handleSubmit, onSubmit, cancelForm, setValue } = useCreateForm(toggleModal);
 
   // 선택된 도시의 정보를 담을 상태 변수
   const [selectedCity, setSelectedCity] = React.useState<{ cityName: string; lat: number; lng: number } | null>(null);
@@ -48,27 +48,32 @@ export default function WritePost() {
           ))}
         </div>
 
-        {/* TODO 수정 필요 category별로 나눠야함  */}
         <div className={cx('sub-category-list')}>
           {subCategories.map((subcategory) => (
             <SubCategoryFiltering key={subcategory} category='추천' filteringType='feed' title={subcategory} />
           ))}
         </div>
-        <Input
-          register={register}
+        <PostModalInput
+          required
+          // register={register}
+          {...register}
           labelName='제목'
           id='title'
           name='title'
           placeholder='제목은 필수입니다'
           maxLength={20}
+          errors={errors}
         />
-        <Input
-          register={register}
+        <PostModalInput
+          required
+          // register={register}
+          {...register}
           labelName='내용'
           id='content'
           name='content'
-          placeholder='내용을 입력하세요'
+          placeholder='내용을 입력해주세요'
           maxLength={20}
+          errors={errors}
         />
         <p className={cx('city-title')}> 어디에 관한 글인가요? (도시까지)</p>
         <AutoCompleteCity
@@ -87,9 +92,8 @@ export default function WritePost() {
       {/* 선택된 도시의 정보를 hidden input으로 추가하여 폼 데이터에 포함 */}
       {selectedCity && (
         <>
-          {/* <input type='hidden' {...register('cityName')} value={selectedCity.cityName} /> */}
-          <input type='hidden' {...register('lat')} value={selectedCity.lat} />
-          <input type='hidden' {...register('lng')} value={selectedCity.lng} />
+          <input type='hidden' {...register('lat')} value={selectedCity?.lat} />
+          <input type='hidden' {...register('lng')} value={selectedCity?.lng} />
         </>
       )}
     </form>
