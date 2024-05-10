5577ab4da47d52fbc0807c2e0fd219d5715b04e4
yejiniee <02doyoung01@naver.com>
2024-05-10 16:30:18 +0900

    remove: 안 쓰는 파일 삭제

--- apis/user/updateBio.ts
+++
@@ -1,8 +0,0 @@
-import instance from '@apis/axios';
-
-// TODO:  dto의 타입 확인.
-export default async function updateBio(userId: number) {
-  const result = await instance.patch(`/user/${userId}/bio`);
-
-  return result.data;
-}
