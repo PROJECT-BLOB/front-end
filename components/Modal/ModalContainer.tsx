187fc378f297c69338e41b0e93d70f2f23ec3028
chlangus <angusangus11@gmail.com>
2024-05-10 22:42:04 +0900

    fix: 모달 띄웠을 때 뒷배경 스크롤 안되게끔

--- components/Modal/ModalContainer.tsx
+++ components/Modal/ModalContainer.tsx
@@ -23,6 +23,12 @@ const ModalList: { [key in ModalName]: JSX.Element } = {
 export default function ModalContainer() {
   const { isOpen, name } = useModalStore();
 
+  if (isOpen) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+
   return (
     isOpen && (
       <Portal>
