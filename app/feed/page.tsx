d26138d08a72273337d85f9f47ffa3eb990480b5
chlangus <angusangus11@gmail.com>
2024-05-13 20:18:13 +0900

    feat: 도시 검색 피드 연결

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { APIProvider } from '@vis.gl/react-google-maps';
@@ -8,6 +8,7 @@ import Image from 'next/image';
 
 import searchIcon from '@public/icons/search-icon-gray.svg';
 import settingIcon from '@public/icons/settings-04.svg';
+import { useMapStore } from '@stores/useMapStore';
 import useModalStore, { ModalName } from '@stores/useModalStore';
 
 import BackToTopButton from '@components/BackToTopButton/BackToTopButton';
@@ -27,8 +28,8 @@ const ORDERS = {
 };
 
 export interface filteredData {
-  cityLat: number;
-  cityLng: number;
+  cityLat?: number;
+  cityLng?: number;
   sortBy: Order;
   categories: string;
   startDate: string;
@@ -42,12 +43,13 @@ export interface filteredData {
 }
 
 export default function Feed() {
+  const { lastSearchCity } = useMapStore();
   const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || '';
 
-  // TO DO: 기본 값 설정
+  // 기본 값
   const [filteredData, setFilteredData] = useState<filteredData>({
-    cityLat: 37.5518911,
-    cityLng: 126.9917937,
+    cityLat: lastSearchCity.location?.lat,
+    cityLng: lastSearchCity.location?.lng,
     sortBy: 'recent',
     categories: '',
     startDate: '',
@@ -57,11 +59,8 @@ export default function Feed() {
     minLikes: 0,
     keyword: '',
   });
-  // const [countryAndCity, setCountryAndCity] = useState({ city: '서울', country: '대한민국' });
   const { toggleModal, setCurrentName } = useModalStore();
-
   const [categoryList, setCategoryList] = useState<string[][]>(stringCategoryListToArray(filteredData.categories));
-
   const { register, handleSubmit } = useForm<{ keyword: string }>();
 
   function handleClickModal(name: ModalName) {
@@ -96,24 +95,24 @@ export default function Feed() {
     return arrayCategory.map((category) => category.join(':')).join(',');
   }
 
-  // const handleOnSelectCity = (address: string) => {
-  //   let countryAndCity = address.split(' ');
-
-  //   if (countryAndCity.length >= 3) countryAndCity = [countryAndCity[0], countryAndCity[countryAndCity.length - 1]];
-
-  //   setCountryAndCity({ country: countryAndCity[0], city: countryAndCity[1] });
-  // };
+  // 검색 결과 달라질때마다 필터링 적용
+  useEffect(() => {
+    setFilteredData((previous) => ({
+      ...previous,
+      cityLat: lastSearchCity.location?.lat,
+      cityLng: lastSearchCity.location?.lng,
+    }));
+  }, [lastSearchCity]);
 
   return (
     <main className={styles.feed}>
       <section className={styles['search-country-and-filtering-container']}>
         <div>
-          <span className={styles['search-mention']}>
-            <APIProvider apiKey={GOOGLE_MAP_API_KEY}>
-              <Autocomplete />
-              <BlobMap isDisplaying={false} />
-            </APIProvider>
-          </span>
+          <APIProvider apiKey={GOOGLE_MAP_API_KEY}>
+            <Autocomplete />
+            <BlobMap isDisplaying={false} />
+          </APIProvider>
+          <span className={styles['search-mention']}>{`${lastSearchCity.country} ${lastSearchCity.city}`}</span>
         </div>
         <div className={styles['filtering-container']}>
           <div className={styles['filtering-button-wrapper']}>
