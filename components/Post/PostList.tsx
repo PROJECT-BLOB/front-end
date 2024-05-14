e9e0d6f57ac60c3d9c246d4d9b0a04c98752a500
yejiniee <02doyoung01@naver.com>
2024-05-14 18:19:23 +0900

    feat: 게시글 없는 경우 cta 버튼 보여줌

--- components/Post/PostList.tsx
+++ components/Post/PostList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import { useEffect } from 'react';
 
 import classNames from 'classnames/bind';
@@ -6,6 +7,8 @@ import { filteredData } from '@/app/feed/page';
 import CommentItem from '@/app/mypage/_components/Comment/CommentItem';
 import { Comment, Post } from '@/types/Post';
 import { useFetchBookmarkList, useFetchCommentList, useFetchFeedList, useFetchPostList } from '@queries/usePostQueries';
+
+import CtaComponent from '@components/CtaComponent/CtaComponent';
 
 import PostItem from './PostItem';
 import styles from './PostList.module.scss';
@@ -64,15 +67,23 @@ export default function PostList({ blobId, selectedTab, filteredData }: GetPostL
 
   const postsPages = postsData?.pages ?? [];
 
+  console.log('postsPages', postsPages);
+
   return (
     <div className={cx('container')}>
-      {fetchDataFunction === useFetchCommentList
-        ? postsPages.map((postsPage: { data: { content: Comment[] } }) =>
+      {postsPages[0]?.data?.content?.length > 0 ? (
+        fetchDataFunction === useFetchCommentList ? (
+          postsPages.map((postsPage: { data: { content: Comment[] } }) =>
             postsPage.data.content.map((comment) => <CommentItem key={comment.commentId} comment={comment} />),
           )
-        : postsPages.map((postsPage: { data: { content: Post[] } }) =>
+        ) : (
+          postsPages.map((postsPage: { data: { content: Post[] } }) =>
             postsPage.data.content.map((post: Post) => <PostItem key={post.postId} post={post} />),
-          )}
+          )
+        )
+      ) : (
+        <CtaComponent />
+      )}
       {/*  TODO 로딩 인디케이터 추가 */}
 
       {isFetchingNextPage ? <div>로딩 중...</div> : <div ref={ref} />}
