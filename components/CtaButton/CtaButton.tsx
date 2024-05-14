53f4cb345ce8bb7e06d40cfb5d1f0b81cc48f7e0
yejiniee <02doyoung01@naver.com>
2024-05-14 09:31:43 +0900

    style: cta버튼 제작

---
+++ components/CtaButton/CtaButton.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from 'react';
+
+import classNames from 'classnames/bind';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import Image from 'next/image';
+
+import styles from './CatButton.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface CtaButtonProps {
+  isSidebar?: boolean;
+  imageSource: string | StaticImport;
+  onClick?: () => void;
+}
+
+export default function CtaButton({ children, isSidebar, imageSource, onClick }: PropsWithChildren<CtaButtonProps>) {
+  return (
+    <button type='button' className={cx(isSidebar ? 'container-sidebar' : 'container-default')} onClick={onClick}>
+      <div className={cx('icon')}>
+        <Image src={imageSource} alt='icon' />
+      </div>
+      <span className={cx('text')}>{children}</span>
+    </button>
+  );
+}
