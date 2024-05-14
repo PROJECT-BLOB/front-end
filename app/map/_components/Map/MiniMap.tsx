4455d07c12325acd955d9fbcff14c289a7b80ad3
INKmin9 <mkkim9901@naver.com>
2024-05-14 20:11:55 +0900

    style: 미니맵 크기 조정, 현위치 버튼 스타일링

--- app/map/_components/Map/MiniMap.tsx
+++ app/map/_components/Map/MiniMap.tsx
@@ -3,13 +3,19 @@ import * as process from 'process';
 import React, { useEffect } from 'react';
 
 import { APIProvider, ControlPosition, Map, MapControl, MapEvent } from '@vis.gl/react-google-maps';
+import classNames from 'classnames/bind';
+import Image from 'next/image';
 
 import MapHandler from '@/app/map/_components/Map/MapHandler';
 import Marker from '@/app/map/_components/Marker/Marker';
 import { MINI_MAP_STYLES } from '@/app/map/_constants/mapOptions';
+import target from '@public/icons/target-03-1.svg';
 import { useMapStore } from '@stores/useMapStore';
 
 import style from './MapTest.module.scss';
+import styles from './MiniMap.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default function MiniMap() {
   const lastMapCenter = useMapStore((state) => state.lastMapCenter);
@@ -47,7 +53,7 @@ export default function MiniMap() {
   useEffect(() => {}, []);
 
   return (
-    <div>
+    <div className={cx('minimap-wrapper')}>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || ''} libraries={['marker']}>
         <div className={style['map-container']}>
           <Map
@@ -66,11 +72,10 @@ export default function MiniMap() {
           <Marker />
         </MapControl>
       </APIProvider>
-      <button
-        type='button'
-        onClick={handleGetCurrentPosition}
-        style={{ width: '20px', height: '20px', background: 'red' }}
-      />
+      <button type='button' onClick={handleGetCurrentPosition} className={cx('current-location-button')}>
+        <Image src={target} width={16} height={16} alt='target' />
+        현재 내 위치
+      </button>
     </div>
   );
 }
