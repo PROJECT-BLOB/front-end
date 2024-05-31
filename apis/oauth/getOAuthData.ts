e4c49f100dab4b33eccde60aa476b597044bef30
yejiniee <02doyoung01@naver.com>
2024-05-31 16:57:41 +0900

    fix: 개발용 로그인 api

--- apis/oauth/getOAuthData.ts
+++ apis/oauth/getOAuthData.ts
@@ -11,7 +11,8 @@ export default async function getOAuthData(
   providerType: 'naver' | 'kakao' | 'google',
   code: string | null,
 ): Promise<{ data: OAuthToken; status: number }> {
-  const { data, status } = await instance.get(`${BASE_URL}/oauth/${providerType}/callback?code=${code}`);
+  // const { data, status } = await instance.get(`${BASE_URL}/oauth/${providerType}/callback?code=${code}`);
+  const { data, status } = await instance.get(`${BASE_URL}/oauth/local/${providerType}/callback?code=${code}`);
 
   return { data, status };
 }
