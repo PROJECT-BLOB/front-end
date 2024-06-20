fe616ea37dd37b608bfc13b5e088947f5fc460b8
chlangus <angusangus11@gmail.com>
2024-06-20 13:37:16 +0900

    fix: 실행 환경에 따라 로그인 api 요청 다르게

--- apis/oauth/getOAuthData.ts
+++ apis/oauth/getOAuthData.ts
@@ -7,11 +7,15 @@ interface OAuthToken {
   refreshToken: string;
 }
 
+export const isLocal = window.location.hostname.includes('localhost') || window.location.hostname.includes('127.0.0.1');
+
 export default async function getOAuthData(
   providerType: 'naver' | 'kakao' | 'google',
   code: string | null,
 ): Promise<{ data: OAuthToken; status: number }> {
-  const { data, status } = await instance.get(`${BASE_URL}/oauth/${providerType}/callback?code=${code}`);
+  const { data, status } = isLocal
+    ? await instance.get(`${BASE_URL}/oauth/local/${providerType}/callback?code=${code}`)
+    : await instance.get(`${BASE_URL}/oauth/${providerType}/callback?code=${code}`);
   // const { data, status } = await instance.get(`${BASE_URL}/oauth/local/${providerType}/callback?code=${code}`);
 
   return { data, status };
